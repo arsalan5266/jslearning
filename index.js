1798054c9d038fd303b8c8cb2ae3576d8d29a9d9
arsalan5266 <muhammad.arsalan4545@gmail.com>
2024-03-01 12:13:36 +0500

    template literals

--- index.js
+++ index.js
@@ -23,17 +23,60 @@
 // console.log(info)
 
 // localStorage.clear();
-const person = {
- firstName:"John",
- lastName:"Doe", 
- age:50,
- eyeColor:"blue",
-}
-let obj = JSON.stringify(person)
-console.log(obj);
+// const person = {
+//  firstName:"John",
+//  lastName:"Doe", 
+//  age:50,
+//  eyeColor:"blue",
+// }
+// let obj = JSON.stringify(person)
+// console.log(obj);
+
+
+// let newObj = JSON.parse(obj)
+// console.log(newObj);
+
+// // template literal
+// let FrstName = "john";
+// let LstName = "Doe";
+// let text = `Welcom ${FrstName},${LstName}`;
+// console.log(text);
 
 
 
+let name ="rameez"
+let age =22
+// console.log("my name is " + name + " my age is " + age);
 
-let newObj = JSON.parse(obj)
-console.log(newObj);
+console.log(`my name is ${name} and my age is ${age}`);
+
+
+let todayDate= new Date()
+console.log(todayDate);
+
+//array  methods
+//  let fruits = ["banana","mango","apple","orsngr"]
+//  let size = fruits.length;
+//  console.log(fruits);
+
+
+// let numbers = [2,3,4,5,6,7,8,]
+//  let number = numbers.toString();
+//  console.log(number);
+
+//  const boy = ["name","Arsalan"];
+//  const boyr = ["Age",24];
+//  const res = boy.concat(boyr)
+//  console.log(res);
+// odd condition
+//  if(numbers[i]%==!0){
+
+// }
+let numbers = [2,3,4,5,6,7,8,];
+for (let i = 0; i < numbers.length; i++) {
+ if (numbers[i]%2==0) {
+    console.log(numbers[i],"even")
+
+   
+ }      
+   }
